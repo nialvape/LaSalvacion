87b2f26da341e3277c6e3463b622a4f9719015c6
nialvape <joacopenialva@gmail.com>
2021-09-28 00:03:56 -0300

    primer commit desde vscode

--- codigo.js
+++ codigo.js
@@ -1,10 +1,14 @@
 const btn_menu = document.querySelector(".menu_btn"),
       menu = document.querySelector(".menu"),
-      btn_open_menu = document.querySelector(".open_menu_btn")
+      btn_open_menu = document.querySelector(".open_menu_btn"),
+      main_container = document.querySelector(".main-container");
 
 btn_menu.addEventListener("click", (activeMenu)=>{
-    menu.classList.toggle("active_menu");
+    menu.classList.add("active_menu");
 });
 btn_open_menu.addEventListener("click", (activeMenu)=>{
-    menu.classList.toggle("active_menu");
+    menu.classList.remove("active_menu");
 });
+main_container.addEventListener("click", (activeMenu)=>{
+    menu.classList.remove("active_menu");
+})
